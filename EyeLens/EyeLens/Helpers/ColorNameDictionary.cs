4126bb209da7b677441a025b100484aabfa49135
Denis <dendibakh@gmail.com>
2013-11-24 01:44:39 +0400

    Add min difference value calculator

--- EyeLens/EyeLens/Helpers/ColorNameDictionary.cs
+++ EyeLens/EyeLens/Helpers/ColorNameDictionary.cs
@@ -22,7 +22,18 @@ namespace EyeLens.Helpers
                              .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
                              .ToArray();
         }
+        private struct RGBColor
+        {
+            public byte R;
+            public byte G;
+            public byte B;
+        };
 
+        private static int GetValueDiff(RGBColor c1, RGBColor c2)
+        {
+            return Math.Abs(c1.R - c2.R) + Math.Abs(c1.G - c2.G) + Math.Abs(c1.B - c2.B);
+        }
+            
         /// <summary>
         /// Get nearest name for color
         /// </summary>
@@ -33,19 +44,30 @@ namespace EyeLens.Helpers
         public static string GetColorName(byte R, byte G, byte B)
         {
             string _outValue = "";
+            RGBColor grabbedColor = new RGBColor();
+            grabbedColor.R = R;
+            grabbedColor.G = G;
+            grabbedColor.B = B;
             try
             {
                 string jsonStr = AppResources.jsonColorData.Replace("\\", "");
+                var minDiff = Int32.MaxValue;
                 var data = JObject.Parse(jsonStr);
                 foreach(var item in data)
                 {
                     string key = item.Key.ToString();
                     byte[] colorCode = ColorNameDictionary.StringToByteArray(key);
-                    byte Rcode = colorCode[0];
-                    byte Gcode = colorCode[1];
-                    byte Bcode = colorCode[2];
+                    RGBColor c = new RGBColor();
+                    c.R = colorCode[0];
+                    c.G = colorCode[1];
+                    c.B = colorCode[2];
 
-                    string value = item.Value.ToString();
+                    int curDiff = GetValueDiff(grabbedColor, c);
+                    if (minDiff > curDiff)
+                    {
+                        minDiff = curDiff;
+                        _outValue = item.Value.ToString();
+                    }
                 }
             }
             catch { };
