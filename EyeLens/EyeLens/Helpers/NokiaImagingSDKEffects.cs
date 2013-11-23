f82439b499334a818ca6a88ded21a1e70733ef35
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-11-23 13:34:56 +0400

    Added nokia Imaging SDK

---
+++ EyeLens/EyeLens/Helpers/NokiaImagingSDKEffects.cs
@@ -0,0 +1,246 @@
+﻿/*
+ * Copyright © 2013 Nokia Corporation. All rights reserved.
+ * Nokia and Nokia Connecting People are registered trademarks of Nokia Corporation. 
+ * Other product and company names mentioned herein may be trademarks
+ * or trade names of their respective owners. 
+ * See LICENSE.TXT for license information.
+ */
+
+using Nokia.Graphics.Imaging;
+using EyeLens.Resources;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Phone.Media.Capture;
+using Windows.Storage.Streams;
+
+namespace EyeLens
+{
+    public class NokiaImagingSDKEffects : ICameraEffect
+    {
+        private PhotoCaptureDevice _photoCaptureDevice = null;
+        private CameraPreviewImageSource _cameraPreviewImageSource = null;
+        private FilterEffect _filterEffect = null;
+        private CustomEffectBase _customEffect = null;
+        private int _effectIndex = 0;
+        private int _effectCount = 9;
+        private Semaphore _semaphore = new Semaphore(1, 1);
+
+        public String EffectName { get; private set; }
+
+        private string _shortFilterName;
+        /// <summary>
+        /// short filter name
+        /// </summary>
+        public string ShortFilterName
+        {
+            get { return _shortFilterName; }
+            set { 
+                _shortFilterName = value;
+
+            }
+        }
+        
+
+        public PhotoCaptureDevice PhotoCaptureDevice
+        {
+            set
+            {
+                if (_photoCaptureDevice != value)
+                {
+                    while (!_semaphore.WaitOne(100));
+
+                    _photoCaptureDevice = value;
+
+                    Initialize();
+
+                    _semaphore.Release();
+                }
+            }
+        }
+
+        ~NokiaImagingSDKEffects()
+        {
+            while (!_semaphore.WaitOne(100));
+
+            Uninitialize();
+
+            _semaphore.Release();
+        }
+
+        public async Task GetNewFrameAndApplyEffect(IBuffer frameBuffer, Size frameSize)
+        {
+            if (_semaphore.WaitOne(500))
+            {
+                var scanlineByteSize = (uint)frameSize.Width * 4; // 4 bytes per pixel in BGRA888 mode
+                var bitmap = new Bitmap(frameSize, ColorMode.Bgra8888, scanlineByteSize, frameBuffer);
+
+                if (_filterEffect != null)
+                {
+                    var renderer = new BitmapRenderer(_filterEffect, bitmap);
+                    await renderer.RenderAsync();
+                }
+                else if (_customEffect != null)
+                {
+                    var renderer = new BitmapRenderer(_customEffect, bitmap);
+                    await renderer.RenderAsync();
+                }
+                else
+                {
+                    var renderer = new BitmapRenderer(_cameraPreviewImageSource, bitmap);
+                    await renderer.RenderAsync();
+                }
+
+                _semaphore.Release();
+            }
+        }
+
+        public void NextEffect()
+        {
+            if (_semaphore.WaitOne(500))
+            {
+                Uninitialize();
+
+                _effectIndex++;
+
+                if (_effectIndex >= _effectCount)
+                {
+                    _effectIndex = 0;
+                }
+
+                Initialize();
+
+                _semaphore.Release();
+            }
+        }
+
+        public void PreviousEffect()
+        {
+            if (_semaphore.WaitOne(500))
+            {
+                Uninitialize();
+                
+                _effectIndex--;
+
+                if (_effectIndex < 0)
+                {
+                    _effectIndex = _effectCount - 1;
+                }
+
+                Initialize();
+
+                _semaphore.Release();
+            }
+        }
+
+        private void Uninitialize()
+        {
+            if (_cameraPreviewImageSource != null)
+            {
+                _cameraPreviewImageSource.Dispose();
+                _cameraPreviewImageSource = null;
+            }
+
+            if (_filterEffect != null)
+            {
+                _filterEffect.Dispose();
+                _filterEffect = null;
+            }
+
+            if (_customEffect != null)
+            {
+                _customEffect.Dispose();
+                _customEffect = null;
+            }
+        }
+
+        private void Initialize()
+        {
+            var filters = new List<IFilter>();
+            var nameFormat = "{0}/" + _effectCount + " - {1}";
+
+            _cameraPreviewImageSource = new CameraPreviewImageSource(_photoCaptureDevice);
+
+            switch (_effectIndex)
+            {
+                case 0:
+                    {
+                        ShortFilterName = AppResources.Filter_MagicPen;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_MagicPen);
+                        filters.Add(new MagicPenFilter());
+                    }
+                    break;
+
+                case 1:
+                    {
+                        ShortFilterName = AppResources.Filter_Grayscale;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Grayscale);
+                        filters.Add(new GrayscaleFilter());
+                    }
+                    break;
+
+                case 2:
+                    {
+                        ShortFilterName = AppResources.Filter_Antique;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Antique);
+                        filters.Add(new AntiqueFilter());
+                    }
+                    break;
+
+                case 3:
+                    {
+                        ShortFilterName = AppResources.Filter_Stamp;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Stamp);
+                        filters.Add(new StampFilter(4, 0.3));
+                    }
+                    break;
+
+                case 4:
+                    {
+                        ShortFilterName = AppResources.Filter_Grayscale;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Cartoon);
+                        filters.Add(new CartoonFilter(false));
+                    }
+                    break;
+                case 5:
+                    {
+                        ShortFilterName = AppResources.Filter_Sharpness;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Sharpness);
+                        filters.Add(new SharpnessFilter(7));
+                    }
+                    break;
+
+                case 6:
+                    {
+                        ShortFilterName = AppResources.Filter_AutoEnhance;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_AutoEnhance);
+                        filters.Add(new AutoEnhanceFilter());
+                    }
+                    break;
+                case 7:
+                    {
+                        ShortFilterName = AppResources.Filter_None;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_None);
+                    }
+                    break;
+                case 8:
+                    {
+                        ShortFilterName = AppResources.Filter_Custom;
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Custom);
+                        _customEffect = new CustomEffect(_cameraPreviewImageSource);
+                    }
+                    break;
+            }
+
+            if (filters.Count > 0)
+            {
+                _filterEffect = new FilterEffect(_cameraPreviewImageSource)
+                {
+                    Filters = filters
+                };
+            }
+        }
+    }
+}
--- EyeLens/EyeLens/MainPage.xaml.cs
+++ EyeLens/EyeLens/MainPage.xaml.cs
@@ -8,18 +8,160 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using EyeLens.Resources;
+using Windows.Phone.Media.Capture;
+using System.Threading;
+using System.Windows.Media;
+using Windows.Phone.Speech.Synthesis;
 
 namespace EyeLens
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
+        private MediaElement _mediaElement = null;
+        private PhotoCaptureDevice _photoCaptureDevice = null;
+        private NokiaImagingSDKEffects _cameraEffect = null;
+        private CameraStreamSource _cameraStreamSource = null;
+        private Semaphore _cameraSemaphore = new Semaphore(1, 1);
+
         public MainPage()
         {
             InitializeComponent();
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+            ApplicationBar = new ApplicationBar();
+
+            var previousButton = new ApplicationBarIconButton(new Uri("/Assets/Icons/previous.png", UriKind.Relative));
+            previousButton.Text = AppResources.PreviousEffectButtonText;
+            //previousButton.Text = LocalizedStrings["PreviousEffectButtonText"];
+            previousButton.Click += PreviousButton_Click;
+
+            ApplicationBar.Buttons.Add(previousButton);
+
+            var nextButton = new ApplicationBarIconButton(new Uri("/Assets/Icons/next.png", UriKind.Relative));
+            nextButton.Text = AppResources.NextEffectButtonText;
+            nextButton.Click += NextButton_Click;
+
+            ApplicationBar.Buttons.Add(nextButton);
+
+            var aboutMenuItem = new ApplicationBarMenuItem();
+            aboutMenuItem.Text = AppResources.AboutPageButtonText;
+            aboutMenuItem.Click += AboutMenuItem_Click;
+
+            ApplicationBar.MenuItems.Add(aboutMenuItem);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Initialize();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            while (!_cameraSemaphore.WaitOne(100)) ;
+
+            Uninitialize();
+
+            _cameraSemaphore.Release();
+        }
+
+        private async void Initialize()
+        {
+            //Dispatcher.BeginInvoke(async () =>
+            //{
+                StatusTextBlock.Text = AppResources.MainPage_StatusTextBlock_StartingCamera;
+
+                var resolution = PhotoCaptureDevice.GetAvailablePreviewResolutions(CameraSensorLocation.Back).Last();
+
+                _photoCaptureDevice = await PhotoCaptureDevice.OpenAsync(CameraSensorLocation.Back, resolution);
+
+                await _photoCaptureDevice.SetPreviewResolutionAsync(resolution);
+
+                _cameraEffect = new NokiaImagingSDKEffects();
+                _cameraEffect.PhotoCaptureDevice = _photoCaptureDevice;
+
+                _cameraStreamSource = new CameraStreamSource(_cameraEffect, resolution);
+                _cameraStreamSource.FrameRateChanged += CameraStreamSource_FPSChanged;
+
+                _mediaElement = new MediaElement();
+                _mediaElement.Stretch = Stretch.UniformToFill;
+                _mediaElement.BufferingTime = new TimeSpan(0);
+                _mediaElement.SetSource(_cameraStreamSource);
+
+                // Using VideoBrush in XAML instead of MediaElement, because otherwise
+                // CameraStreamSource.CloseMedia() does not seem to be called by the framework:/
+
+                BackgroundVideoBrush.SetSource(_mediaElement);
+
+                StatusTextBlock.Text = _cameraEffect.EffectName;
+            //});
+        }
+
+        private void Uninitialize()
+        {
+            StatusTextBlock.Text = "";
+
+            if (_mediaElement != null)
+            {
+                _mediaElement.Source = null;
+                _mediaElement = null;
+            }
+
+            if (_cameraStreamSource != null)
+            {
+                _cameraStreamSource.FrameRateChanged -= CameraStreamSource_FPSChanged;
+                _cameraStreamSource = null;
+            }
+
+            _cameraEffect = null;
+
+            if (_photoCaptureDevice != null)
+            {
+                _photoCaptureDevice.Dispose();
+                _photoCaptureDevice = null;
+            }
+        }
+
+        private void AboutMenuItem_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AboutPage.xaml", UriKind.Relative));
+        }
+
+        private async void NextButton_Click(object sender, EventArgs e)
+        {
+            _cameraEffect.NextEffect();
+
+            StatusTextBlock.Text = _cameraEffect.EffectName;
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            await synth.SpeakTextAsync(AppResources.NextEffectButtonText + _cameraEffect.ShortFilterName);
+
+        }
+
+        private async void PreviousButton_Click(object sender, EventArgs e)
+        {
+            _cameraEffect.PreviousEffect();
+
+            StatusTextBlock.Text = _cameraEffect.EffectName;
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            await synth.SpeakTextAsync(AppResources.PreviousEffectButtonText + _cameraEffect.ShortFilterName);
+        }
+
+        private void CameraStreamSource_FPSChanged(object sender, int e)
+        {
+            FrameRateTextBlock.Text = String.Format(AppResources.MainPage_FrameRateTextBlock_Format, e);
+        }
+
+        private async void LayoutRoot_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (_cameraSemaphore.WaitOne(100))
+            {
+                await _photoCaptureDevice.FocusAsync();
+
+                _cameraSemaphore.Release();
+            }
         }
 
         // Sample code for building a localized ApplicationBar
